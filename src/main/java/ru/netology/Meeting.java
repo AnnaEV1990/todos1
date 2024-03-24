50d5b567cd90b05b9d005122a8955f70d055b2d1
Anna Evseykina <anytta9032@yandex.ru>
2024-03-24 11:19:19 +0700

    change all fails.And add TaskTest1

--- src/main/java/ru/netology/Meeting.java
+++ src/main/java/ru/netology/Meeting.java
@@ -24,12 +24,7 @@ public class Meeting extends Task {
         return start;
     }
 
-    // public Meeting(int id, String topic, String project, String start) {
-    //     super(id);
-    //     this.topic = topic;
-    //     this.project = project;
-    //      this.start = start;
-    // }
+
 
     @Override
     public boolean matches(String query) {
